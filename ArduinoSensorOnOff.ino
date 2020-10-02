82a31f0d60087126f396ac58bcfdca1b7fea31da
Jordan Alba <jordan008@live.ca>
2020-10-02 12:11:52 -0400

    Initial Commit

---
+++ ArduinoSensorOnOff.ino
@@ -0,0 +1,18 @@
+int sensorPin = A0;
+int sensorValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+
+}
+
+void loop() {
+  sensorValue = analogRead( sensorPin);
+  Serial.println(sensorValue);
+  //Serial.println(" ");
+
+
+  delay(50);
+ 
+}
